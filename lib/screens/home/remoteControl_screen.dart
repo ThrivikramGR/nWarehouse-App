4b58b36ee87e3917259c6d88faa761e31d46469e
Thrivikram G R <vikramthiru02@outlook.com>
2022-11-25 09:31:04 +0530

    remote control layout added, onKeepPressed listeners manually implemented

--- lib/screens/home/remoteControl_screen.dart
+++ lib/screens/home/remoteControl_screen.dart
@@ -8,6 +8,13 @@ class RemoteControlScreen extends StatefulWidget {
 }
 
 class _RemoteControlScreenState extends State<RemoteControlScreen> {
+  Map<Direction, bool> pressStateMap = {
+    Direction.UP: false,
+    Direction.DOWN: false,
+    Direction.LEFT: false,
+    Direction.RIGHT: false,
+  };
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,6 +35,110 @@ class _RemoteControlScreenState extends State<RemoteControlScreen> {
           color: Color(0xFF323232),
         ),
       ),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            GestureDetector(
+              onTapDown: (TapDownDetails val) async {
+                pressStateMap[Direction.LEFT] = true;
+                do {
+                  print("left Pressed");
+                  await Future.delayed(Duration(seconds: 1));
+                } while (pressStateMap[Direction.LEFT] == true);
+              },
+              onTapCancel: () {
+                pressStateMap[Direction.LEFT] = false;
+              },
+              child: InkWell(
+                onTap: () {},
+                child: Padding(
+                  padding: EdgeInsets.all(50),
+                  child: Icon(
+                    Icons.arrow_back,
+                  ),
+                ),
+              ),
+            ),
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                GestureDetector(
+                  onTapDown: (TapDownDetails val) async {
+                    pressStateMap[Direction.UP] = true;
+
+                    do {
+                      print("up Pressed");
+                      await Future.delayed(Duration(seconds: 1));
+                    } while (pressStateMap[Direction.UP] == true);
+                  },
+                  onTapCancel: () {
+                    pressStateMap[Direction.UP] = false;
+                  },
+                  child: InkWell(
+                    onTap: () {},
+                    child: Padding(
+                      padding: EdgeInsets.all(50),
+                      child: Icon(
+                        Icons.arrow_upward,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 200,
+                ),
+                GestureDetector(
+                  onTapDown: (TapDownDetails val) async {
+                    pressStateMap[Direction.DOWN] = true;
+
+                    do {
+                      print("down Pressed");
+                      await Future.delayed(Duration(seconds: 1));
+                    } while (pressStateMap[Direction.DOWN] == true);
+                  },
+                  onTapCancel: () {
+                    pressStateMap[Direction.DOWN] = false;
+                  },
+                  child: InkWell(
+                    onTap: () {},
+                    child: Padding(
+                      padding: EdgeInsets.all(50),
+                      child: Icon(
+                        Icons.arrow_downward,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            GestureDetector(
+              onTapDown: (TapDownDetails val) async {
+                pressStateMap[Direction.RIGHT] = true;
+
+                do {
+                  print("right Pressed");
+                  await Future.delayed(Duration(seconds: 1));
+                } while (pressStateMap[Direction.RIGHT] == true);
+              },
+              onTapCancel: () {
+                pressStateMap[Direction.RIGHT] = false;
+              },
+              child: InkWell(
+                onTap: () {},
+                child: Padding(
+                  padding: EdgeInsets.all(50),
+                  child: Icon(
+                    Icons.arrow_forward,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
+
+enum Direction { UP, DOWN, LEFT, RIGHT }
